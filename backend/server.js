a378a97a9d4b24e2fb5d971ed2d2062e06e0decd
Arshak <arshakmohammed004@gmail.com>
2022-11-10 13:11:00 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app=express()
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+app.listen(port,()=>{
+    console.log(`Server started on port:${port}`)
+})
